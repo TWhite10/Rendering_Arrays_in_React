3d2ddbcd6f7214d5a8d0b58b9d3282aa55639760
TWhite10 <136852211+TWhite10@users.noreply.github.com>
2024-12-06 17:05:19 -0500

    display commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import { useState } from 'react'
-import Learner from './components/Learner.jsx'
-import score from './components/score.jsx'
-import './App.css'
+import { useState } from 'react';
+import Learner from './components/Learner.jsx';
+import LearnerData from './LearnerData.jsx';
+import './App.css';
 
 function App() {
- 
+
+ const [LearnerData, setLearnerData] = useState({
+  Learners: [
+    {
+      name: 'Cait Yomorta',
+      bio: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus placeat nostrum explicabo? Voluptatibus expedita saepe officia optio, commodi totam ratione laudantium ipsum porro molestias, quasi nulla minus vitae laboriosam corrupti Delectus inventore explicabo est odit incidunt rem a recusandae eum pariatur. Aperiam doloremque blanditiis harum voluptate animi fugit beatae asperiores quo, dignissimos sed illum veniam eum accusantium nulla quod voluptatum',
+      scores: [
+        {
+          date: '2018-02-08',
+          score: 77
+        },
+        {
+          date: '2018-04-22',
+          score: 92
+        },
+        {
+          date: '2018-09-15',
+          score: 68
+        }
+      ]
+    }]
+  })
   return (
     <>
+    <h1>Learners </h1>
+    <Learner Learner={LearnerData.Learners[0]}/>
 
     </>
   )
---
+++ src/components/Learner.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+//import Score from './components/Score.jsx'
+//import './App.css'
+
+function Learner({Learner}) {
+ 
+  return (
+    <>
+    <h2>{Learner.name}</h2>
+    <p>{Learner.bio}</p>
+    </>
+  )
+}
+
+export default Learner
