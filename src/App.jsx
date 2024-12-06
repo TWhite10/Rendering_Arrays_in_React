8927ef29d7f9078c98c4aab5bb87809dc5cecf0e
TWhite10 <136852211+TWhite10@users.noreply.github.com>
2024-12-06 16:01:09 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Learner from './components/Learner.jsx'
+import score from './components/score.jsx'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
